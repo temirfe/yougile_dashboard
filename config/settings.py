8895ced98f4b17f4d394ad656d6af6f4027e591f
Temirbek <temirbek@gmail.com>
2025-08-12 15:49:40 +0600

    misc

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'yougile',
-    'rest_framework'
+    'main'
 ]
 
 MIDDLEWARE = [
