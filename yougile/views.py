b58e98b01bbf759388aaa7134e156008e95915a0
Temirbek <temirbek@gmail.com>
2025-08-13 18:17:44 +0600

    misc

--- yougile/views.py
+++ yougile/views.py
@@ -8,26 +8,40 @@ from yougile.services.task_service import fetch_and_save_all_companies_tasks, fe
 from django.http import HttpResponse
 
 def fetch_yougile_projects(request):
-    #context = fetch_and_save('prosoft')
-    context = fetch_and_save_all_companies_projects()
+    if 'company' in request.GET:
+        company = request.GET['company']
+        context = fetch_and_save_projects(company)
+    else:
+        context = fetch_and_save_all_companies_projects()
 
     return HttpResponse(f'nahoi {context}')
 
 def fetch_yougile_boards(request):
     #context = fetch_and_save_boards('product')
-    context = fetch_and_save_all_companies_boards()
+    if 'company' in request.GET:
+        company = request.GET['company']
+        context = fetch_and_save_boards(company)
+    else:
+        context = fetch_and_save_all_companies_boards()
 
     return HttpResponse(f'fetch_yougile_boards {context}')
 
 def fetch_yougile_columns(request):
     #context = fetch_and_save_columns('product', 'b41008ff-a281-4000-abf3-310a6ee48c19')
-    #context = fetch_and_save_columns('prosoft')
-    context = fetch_and_save_all_companies_columns()
+    if 'company' in request.GET:
+        company = request.GET['company']
+        context = fetch_and_save_columns(company)
+    else:
+        context = fetch_and_save_all_companies_columns()
 
     return HttpResponse(f'fetch_yougile_columns {context}')
 
 def fetch_yougile_users(request):
-    context = fetch_and_save_all_companies_users()
+    if 'company' in request.GET:
+        company = request.GET['company']
+        context = fetch_and_save_users(company)
+    else:
+        context = fetch_and_save_all_companies_users()
     return HttpResponse(f'fetch users {context}')
 
 def fetch_yougile_tasks(request):
